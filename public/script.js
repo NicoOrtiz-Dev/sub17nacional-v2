43e7be3cd3f79317b9255f9461084e9c01092be2
NicoX <ng054235@gmail.com>
2025-06-27 20:17:45 -0500

    Archivos editados

--- public/script.js
+++ public/script.js
@@ -1,11 +1,26 @@
-let golesN = 0;
-let golesA = 0;
 
-const pusher = new Pusher("32360ed1be476bc06d0e", {
-  cluster: "mt1"
-});
-
+const pusher = new Pusher("32360ed1be476bc06d0e", { cluster: "mt1" });
 const channel = pusher.subscribe("partido");
+let golesN = 0, golesA = 0;
+
+function vibrar(ms=500){ if(navigator.vibrate) navigator.vibrate(ms); }
+
+function mostrar(texto, tipo){
+  const div = document.createElement("div"); div.className = "alerta";
+
+  if(tipo === "gol") {
+    const img = document.createElement("img");
+   img.src = texto.includes("Nacional")
+  ? "img/nacional.png"
+  : "img/america.png";
+    img.style.width = "60px";
+    div.append(img);
+    vibrar(800);
+  }
+
+  div.innerHTML += `<div>${texto}</div>`;
+  document.getElementById("notificaciones").prepend(div);
+}
 
 channel.bind("evento", (data) => {
   const { tipo, detalle, jugador } = data;
@@ -17,19 +32,22 @@ channel.bind("evento", (data) => {
       golesA++;
     }
 
-    document.getElementById("marcadorTexto").innerText = `${golesN} - ${golesA}`;
-    mostrar(`⚽ ¡Gol de ${detalle}!<br/>👟 Lo hizo: ${jugador || "Desconocido"}`, "gol");
+    document.getElementById("marcador").innerText = `${golesN} - ${golesA}`;
 
-    if (navigator.vibrate) {
-      navigator.vibrate(500);
-    }
+    mostrar(
+      `⚽ ¡Gol de ${detalle}!<br/>👟 Lo hizo: ${jugador || "Desconocido"}`,
+      "gol"
+    );
+  } else {
+    mostrar(`🔔 ${detalle}`, "evento");
   }
 });
 
-function mostrar(texto, tipo) {
-  const div = document.getElementById("eventos");
-  const evento = document.createElement("div");
-  evento.className = tipo;
-  evento.innerHTML = texto;
-  div.prepend(evento);
-}
+
+let s = 0;
+setInterval(() => {
+  s++;
+  const m = String(Math.floor(s / 60)).padStart(2, "0"),
+        sg = String(s % 60).padStart(2, "0");
+  document.getElementById("reloj").innerText = `${m}:${sg}`;
+}, 1000);
